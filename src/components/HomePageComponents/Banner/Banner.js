c4f52e33768aa49a818a95843798a7748d8fb5f7
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 21:04:05 +0600

    Убрал точку в конце из заголовка

--- src/components/HomePageComponents/Banner/Banner.js
+++ src/components/HomePageComponents/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 const Banner = ({clicked}) => {
     return (
         <section className="Banner">
-            <h1 className="Banner__title">Lorem ipsum dolor sit amet.</h1>
+            <h1 className="Banner__title">Lorem ipsum dolor sit amet</h1>
             <p className="Banner__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et impedit, itaque optio ratione similique.</p>
             <button type="button" className="Banner__btn" onClick={clicked}>More info</button>
         </section>
