6a8531af1297f95c7f1caab3bd0fea9c13d11386
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 19:40:30 +0600

    Создал контейнер для страницы с турами

---
+++ src/containers/ProductsPage/ProductsPage.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import './ProductsPage.css';
+import Products from "../../components/ProductsPageComponents/Products/Products";
+import {nanoid} from "nanoid";
+import canyon from "../../assets/img/grand-canyon.jpg";
+import niagaraFalls from "../../assets/img/niagara-falls.jpg";
+import everest from "../../assets/img/everest.jpg";
+import aplineLake from "../../assets/img/apline-lake.jpg";
+
+const ProductsPage = () => {
+    const [product] = useState([
+        {
+            name: 'Grand Canyon',
+            pathImage: canyon,
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ipsam nulla repellat suscipit.',
+            id: nanoid()
+        },
+        {
+            name: 'Niagara Falls',
+            pathImage: niagaraFalls,
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ipsam nulla repellat suscipit.',
+            id: nanoid()
+        },
+        {
+            name: 'Everest',
+            pathImage: everest,
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ipsam nulla repellat suscipit.',
+            id: nanoid()
+        },
+        {
+            name: 'Apline Lake',
+            pathImage: aplineLake,
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ipsam nulla repellat suscipit.',
+            id: nanoid()
+        }
+    ]);
+
+    return (
+        <div className="Products-page">
+            <Products
+                products={product}
+            />
+        </div>
+    );
+};
+
+export default ProductsPage;
