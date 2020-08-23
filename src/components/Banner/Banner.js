45c49513f6333deffae5260d5a3244309fdc4a89
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 14:32:35 +0600

    Сделал баннер главной страницы

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <section className="Banner">
+            <h1 className="Banner__title">Lorem ipsum dolor sit amet.</h1>
+            <p className="Banner__subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et impedit, itaque optio ratione similique.</p>
+            <button type="button" className="Banner__btn">More info</button>
+        </section>
+    );
+};
+
+export default Banner;
