966cf32ada1f8aaaadbd4f234285688bd0cad2e5
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 12:33:03 +0600

    Создал sidebar с навигацией

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Sidebar.css';
+import {NavLink} from "react-router-dom";
+import Logo from "../Logo/Logo";
+
+const Sidebar = () => {
+    return (
+        <aside className="Aside">
+            <Logo />
+            <nav className="Nav">
+                <ul className="Nav-list">
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/"
+                            exact
+                            className="Nav-link"
+                            activeClassName="active-menu"
+                        >Home</NavLink>
+                    </li>
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/products"
+                            className="Nav-link"
+                            activeClassName="active-menu"
+                        >Products</NavLink>
+                    </li>
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/about"
+                            className="Nav-link"
+                            activeClassName="active-menu"
+                        >About Us</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    );
+};
+
+export default Sidebar;
