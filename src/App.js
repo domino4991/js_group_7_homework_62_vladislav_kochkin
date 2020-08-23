aef28a1b9a392f06bd254cd77f1f9422ebae2fd7
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 11:41:21 +0600

    Добавил навигацию по странице

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    NavLink
+} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <nav className="Nav">
+                <ul className="Nav-list">
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/"
+                            exact
+                            activeClassName="active-menu"
+                        >Home</NavLink>
+                    </li>
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/products"
+                            activeClassName="active-menu"
+                        >Products</NavLink>
+                    </li>
+                    <li className="Nav-item">
+                        <NavLink
+                            to="/about"
+                            activeClassName="active-menu"
+                        >About Us</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/" exact>
+                    <p>Home</p>
+                </Route>
+                <Route path="/products" >
+                    <p>products</p>
+                </Route>
+                <Route path="/about">
+                    <p>About</p>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
