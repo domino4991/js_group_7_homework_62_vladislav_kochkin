823f137f8468154443a208107bb1e9ea6608b02d
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-23 16:33:03 +0600

    Контейнер главной страницы

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomePage.css';
+import Banner from "../../components/Banner/Banner";
+import Adventages from "../../components/Adventages/Adventages";
+
+const HomePage = () => {
+
+    return (
+        <main className="Home-page">
+            <Banner
+                clicked={() => window.alert('Who lives at the bottom of the ocean?')}
+            />
+            <Adventages />
+        </main>
+    );
+};
+
+export default HomePage;
